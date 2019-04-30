819649eea229d07eb8377e94f47faaf555610cfe
JHR <jianghairui@sina.cn>
2019-04-30 17:15:03 +0800

    韩驰9张拼图完成

--- application/api/controller/Activity.php
+++ application/api/controller/Activity.php
@@ -88,13 +88,25 @@ class Activity extends Common
                 ->field('c.id, c.title, c.pic, c.pic_back, count(uc.id) as card_amount')
                 ->select();
 
+            // 剩余抽奖次数
             $lucky_draw_times = Db::table('mp_user')
                 ->where([['id', '=', $this->myinfo['uid']]])
                 ->value('lucky_draw_times');
 
+            // 获取用户身份
+            $my = Db::table('mp_user')
+                ->where([['id', '=', $this->myinfo['uid']]])
+                ->find();
+            if ($my['auth'] != 2) {
+                $role = 0;
+            } else {
+                $role = $my['role'];
+            }
+
             $data = [
                 'list' => $list,
-                'lucky_draw_times' => $lucky_draw_times
+                'lucky_draw_times' => $lucky_draw_times,
+                'role' => $role
             ];
             return ajax($data);
         } catch (\Exception $e) {
@@ -127,63 +139,14 @@ class Activity extends Common
                     ->where([['id', '=', $this->myinfo['uid']]])
                     ->setDec('lucky_draw_times', 1);
 
-                $get_gift = 0;  // 0.未获得礼物 1.获得文创礼品 2.获得奖金
                 if ($cid == 5) {
                     // 查看是否得到过现金奖励，未获得过则获得现金奖励
-                    $cash_gift = Db::table('mp_card_cash_gift')
-                        ->where([['uid', '=', $this->myinfo['uid']]])
-                        ->find();
-
-                    if (!$cash_gift) {
-                        $data = [
-                            'uid' => $this->myinfo['uid'],
-                            'money' => number_format(config('card_gift_cash'), 2),
-                            'create_time' => time()
-                        ];
-                        Db::table('mp_card_cash_gift')->insert($data);
-
-                        Db::table('mp_user')
-                            ->where([['id', '=', $this->myinfo['uid']]])
-                            ->setInc('balance', config('card_gift_cash'));
-
-                        $get_gift = 2;
-                    }
+                    $temp_get_gift = $this->check_gift($this->myinfo['uid'], 1);
+                    $get_gift = $temp_get_gift ? $temp_get_gift : 0;
                 } else {
                     // 查看是否获得过文创礼品，没有获得过文创礼品查看是否得到全部8张卡片，如果集齐则获得文创礼品
-                    $cul_gift = Db::table('mp_card_cul_gift')
-                        ->where([['uid', '=', $this->myinfo['uid']]])
-                        ->find();
-                    if (!$cul_gift) {
-                        $w = [
-                            ['uc.uid', '=', $this->myinfo['uid']],
-                            ['c.id', '<>', 5]
-                        ];
-                        $user_has_list = Db::table('mp_card')->alias('c')
-                            ->join('mp_user_card uc', 'uc.cid = c.id')
-                            ->where($w)
-                            ->group('c.id')
-                            ->field('c.id')
-                            ->select();
-
-                        // 如果是8张则为集齐
-                        if (count($user_has_list) == 8) {
-                            // 生成二维码图片
-                            $qr_code_path = 'static/uploads/activity/gift_qrcode/' . md5($this->myinfo['uid'] . rand(1, 1000000000)) . '.png';
-                            $qr_code = create_qrcode($this->myinfo['uid'], $qr_code_path);
-                            if (!$qr_code) {
-                                throw new Exception('生成二维码失败');
-                            }
-
-                            $data = [
-                                'uid' => $this->myinfo['uid'],
-                                'create_time' => time(),
-                                'qrcode' => $qr_code_path
-                            ];
-                            Db::table('mp_card_cul_gift')->insert($data);
-
-                            $get_gift = 1;
-                        }
-                    }
+                    $temp_get_gift = $this->check_gift($this->myinfo['uid'], 2);
+                    $get_gift = $temp_get_gift ? $temp_get_gift : 0;
                 }
                 Db::commit();
             }
@@ -194,17 +157,14 @@ class Activity extends Common
             ];
             return ajax($data);
         } catch (\Exception $e) {
-            // 如果已经生成了二维码图片则删除它
-            if ($qr_code_path) {
-                @unlink($qr_code_path);
-            }
             Db::rollback();
             return ajax($e->getMessage(), -1);
         }
     }
 
     // 生成卡片码（赠送用）
-    public function createCardCode() {
+    public function createCardCode()
+    {
         $post['cid'] = input('post.cid');
         $this->checkPost($post);
 
@@ -247,13 +207,75 @@ class Activity extends Common
         }
     }
 
+    // 通过卡片码获得卡片
+    public function getGiftCard()
+    {
+        $post['card_code'] = input('post.card_code');
+        $this->checkPost($post);
+
+        try {
+            $card = Db::table('mp_user_card')
+                ->where([['card_code', '=', $post['card_code']]])
+                ->find();
+
+            if (!$card) {
+                return ajax('卡片不存在或已被领取', 49);
+            } else if ($card['uid'] == $this->myinfo['uid']) {
+                return ajax('不能领取自己的卡片', 49);
+            }
+
+            $w = [
+                'uid' => $card['uid'],
+                'cid' => $card['cid']
+            ];
+            $this_card_count = Db::table('mp_user_card')
+                ->where($w)
+                ->count();
+
+            if ($this_card_count < 2) {
+                return ajax('用户此张卡片已不足两张，无法领取了', 49);
+            }
+
+            Db::startTrans();
+            $data = [
+                'uid' => $this->myinfo['uid'],
+                'card_code' => ''
+            ];
+            Db::table('mp_user_card')
+                ->where([['id', '=', $card['id']]])
+                ->update($data);
+
+            if ($card['cid'] == 5) {
+                // 查看是否得到过现金奖励，未获得过则获得现金奖励
+                $temp_get_gift = $this->check_gift($this->myinfo['uid'], 1);
+                $get_gift = $temp_get_gift ? $temp_get_gift : 0;
+            } else {
+                // 查看是否获得过文创礼品，没有获得过文创礼品查看是否得到全部8张卡片，如果集齐则获得文创礼品
+                $temp_get_gift = $this->check_gift($this->myinfo['uid'], 2);
+                $get_gift = $temp_get_gift ? $temp_get_gift : 0;
+            }
+            Db::commit();
+
+            $data = [
+                'card_id' => $card['cid'],
+                'get_gift' => $get_gift
+            ];
+            return ajax($data);
+        } catch (\Exception $e) {
+            Db::rollback();
+            return ajax($e->getMessage(), -1);
+        }
+    }
+
     // 文创礼品核销
-    public function culVerify() {
+    public function culVerify()
+    {
         $post = [
             'uid' => input('post.uid'),  // 这个是核销用户的uid
-            'content' => input('post.content')
+
         ];
         $this->checkPost($post);
+        $post['remark'] = input('post.remark', '');
 
         try {
             $cul_gift = Db::table('mp_card_cul_gift')
@@ -276,7 +298,7 @@ class Activity extends Common
 
             $data = [
                 'm_uid' => $this->myinfo['uid'],
-                'content' => $post['content'],
+                'remark' => $post['remark'],
                 'verify_time' => time(),
                 'status' => 1
             ];
@@ -290,9 +312,58 @@ class Activity extends Common
         }
     }
 
-    //
-    public function get_cul() {
+    // 用户文创礼品详情
+    public function getMyCul()
+    {
+        try {
+            $cul_gift = Db::table('mp_card_cul_gift')->alias('ccg')
+                ->join('mp_user u', 'u.id = ccg.m_uid', 'left')
+                ->where([['ccg.uid', '=', $this->myinfo['uid']]])
+                ->field('ccg.id, ccg.uid, ccg.m_uid, ccg.remark, ccg.create_time, ccg.verify_time, ccg.qrcode, ccg.status, u.nickname')
+                ->find();
 
+            if (!$cul_gift) {
+                $cul_gift['status'] = -1;
+            }
+
+            // 卡牌列表
+            $card_list = Db::table('mp_card')
+                ->where([['id', '<>', 5]])
+                ->select();
+
+            $data = [
+                'cul_gift' => $cul_gift,
+                'card_list' => $card_list
+            ];
+
+            return ajax($data);
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
+    }
+
+    /*
+     * 返回文创礼品核销状态
+     *
+     * （循环检测）
+     */
+    public function checkCulStatus()
+    {
+        try {
+            $cul_gift = Db::table('mp_card_cul_gift')
+                ->where([['uid', '=', $this->myinfo['uid']]])
+                ->find();
+
+            if (!$cul_gift) {
+                $cul_gift_status = -1;
+            } else {
+                $cul_gift_status = $cul_gift['status'];
+            }
+
+            return ajax($cul_gift_status);
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
     }
 
     // 测试
@@ -301,8 +372,8 @@ class Activity extends Common
     }
 
     // 测试检查token版
-    public function test_user() {
-        dump($this->myinfo);
+    public function test_user()
+    {
     }
 
     /* 工具方法、私有方法 */
@@ -332,10 +403,12 @@ class Activity extends Common
     {
         $prize_arr = [
             ['id' => 1, 'probability' => 200],
+//            ['id' => 1, 'probability' => 1],
             ['id' => 2, 'probability' => 150],
             ['id' => 3, 'probability' => 100],
             ['id' => 4, 'probability' => 150],
             ['id' => 5, 'probability' => 1],
+//            ['id' => 5, 'probability' => 200],
             ['id' => 6, 'probability' => 199],
             ['id' => 7, 'probability' => 50],
             ['id' => 8, 'probability' => 50],
@@ -369,4 +442,80 @@ class Activity extends Common
         return $result;
     }
 
+    /**
+     * 检查用户是否得到过特定类型奖励，未获得过则获得
+     * $type: 1.文创产品 2.获得奖金
+     *
+     * @throws Exception
+     */
+    private function check_gift($uid, $type) {
+        if ($type == 1) {
+            try {
+                $cash_gift = Db::table('mp_card_cash_gift')
+                    ->where([['uid', '=', $uid]])
+                    ->find();
+
+                if (!$cash_gift) {
+                    $data = [
+                        'uid' => $uid,
+                        'money' => number_format(config('card_gift_cash'), 2),
+                        'create_time' => time()
+                    ];
+                    Db::table('mp_card_cash_gift')->insert($data);
+
+                    Db::table('mp_user')
+                        ->where([['id', '=', $uid]])
+                        ->setInc('balance', config('card_gift_cash'));
+
+                    return 2;
+                }
+            } catch (\Exception $e) {
+                throw new Exception($e->getMessage());
+            }
+        } else {
+            try {
+                $cul_gift = Db::table('mp_card_cul_gift')
+                    ->where([['uid', '=', $uid]])
+                    ->find();
+
+                if (!$cul_gift) {
+                    $w = [
+                        ['uc.uid', '=', $uid],
+                        ['c.id', '<>', 5]
+                    ];
+                    $user_has_list = Db::table('mp_card')->alias('c')
+                        ->join('mp_user_card uc', 'uc.cid = c.id')
+                        ->where($w)
+                        ->group('c.id')
+                        ->field('c.id')
+                        ->select();
+
+                    // 如果是8张则为集齐
+                    if (count($user_has_list) == 8) {
+                        // 生成二维码图片
+                        $qr_code_path = 'static/uploads/activity/gift_qrcode/' . $uid . '_' . md5($uid . rand(1, 1000000000)) . '.png';
+                        $qr_code = create_qrcode($uid, $qr_code_path);
+                        if (!$qr_code) {
+                            throw new Exception('生成二维码失败');
+                        }
+
+                        $data = [
+                            'uid' => $uid,
+                            'create_time' => time(),
+                            'qrcode' => $qr_code_path
+                        ];
+                        Db::table('mp_card_cul_gift')->insert($data);
+
+                        return 1;
+                    }
+                }
+            } catch (\Exception $e) {
+                // 如果已经生成了二维码图片则删除它
+                if ($qr_code_path) {
+                    @unlink($qr_code_path);
+                }
+                throw new Exception($e->getMessage());
+            }
+        }
+    }
 }
