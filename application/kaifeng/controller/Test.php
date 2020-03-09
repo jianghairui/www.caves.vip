94410825bd8e2622e4849cd64ca0084b7438fbad
Jiang <1873645345@qq.com>
2020-03-09 11:34:17 +0800

    KAIFENG TEST

---
+++ application/kaifeng/controller/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang
+ * Date: 2020/3/9
+ * Time: 11:33
+ */
+namespace app\kaifeng\controller;
+
+use think\Controller;
+
+class Test extends Controller {
+
+    public function index() {
+
+    }
+
+}
